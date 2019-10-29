a20cd9aa3a1fc6f60580eb15d5f98d965e0626a3
Jordan Jenkins <JJenkins@ucvts.tec.nj.us>
2019-10-29 10:48:32 -0400

    first commits

--- src/change.js
+++ src/change.js
@@ -3,12 +3,17 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
+let quarters = 0;
+let dimes = 0;
+let nickels = 0;
+let pennies =0;
+
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+  console.log("\nInvalid")  // print your error message here
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+  console.log("\nInvalid") // print your error message here
 } else {
     // write your code for making change here
 }
