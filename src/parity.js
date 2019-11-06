99f10fdabafe5a73821638c41c1a8a58d2bc7e1c
Jordan Jenkins <JJenkins@ucvts.tec.nj.us>
2019-11-06 10:48:41 -0500

    updated commits

--- src/parity.js
+++ src/parity.js
@@ -1,3 +1,13 @@
 const readlineSync = require("readline-sync");
 
 const integer = Number(readlineSync.question("\nEnter an integer: "));
+
+if (!Number.isInteger(integer)) {
+  console.log("\nInvalid.")
+} else if (integer < Number.MIN_SAFE_INTEGER || integer > Number.MAX_SAFE_INTEGER){
+  console.log("\nInvalid.")
+} else if (integer %2==0) {
+  console.log("\nEven.")
+} else {
+  console.log("\nOdd.")
+}
--- src/sequence.js
+++ src/sequence.js
@@ -1,1 +1,33 @@
 const readlineSync = require("readline-sync");
+
+const number = Number(readlineSync.question("\nEnter three numbers."));
+const numbFirst= Number(readlineSync.question("\n"));
+const numbSec = Number(readlineSync.question(""));
+const numbTrd = Number(readlineSync.question(""));
+
+if (Number.isNaN(numbFirst) || Number.isNaN(numbSec) || Number.isNaN(numbTrd)) {
+console.log("\nInvalid.");
+}
+else if ((numbFirst < Number.MIN_SAFE_INTEGER || numbFirst > Number.MAX_SAFE_INTEGER) || (numbSec < Number.MIN_SAFE_INTEGER || numbSec > Number.MAX_SAFE_INTEGER ) || (numbTrd < Number.MIN_SAFE_INTEGER || numbTrd > Number.MAX_SAFE_INTEGER)) {
+  console.log("\nInvalid.");
+}
+else {
+  if (numbFirst<numbSec && numbSec<numbTrd) {
+  console.log("\nStrictly increasing.");
+  }
+  else if (numbFirst<=numbSec && numbSec<=numbTrd) {
+  console.log("\nIncreasing.");
+  }
+  else if (numbFirst==numbSec && numbSec==numbTrd) {
+  console.log("\nEqual.");
+  }
+  else if (numbFirst>=numbSec && numbSec>=numbTrd) {
+  console.log("\nDecreasing.");
+  }
+  else if (numbFirst>numbSec && numbSec>numbTrd) {
+  console.log("\nStrictly decreasing.");
+  }
+  else {
+    console.log("\nUnordered.");
+  }
+}
